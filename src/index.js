bc4bde74d7adf4d2e9d6f88b083465b47d6f6336
Daniel Odeny <daniel.odeny@student.moringaschool.com>
2023-07-12 12:53:22 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  const newTaskDescription = document.querySelector("#new-task-description");
+  const tasksContainer = document.querySelector("#tasks");
+
+  function submitForm(event) {
+    event.preventDefault();
+    const tasks = document.createElement("li");
+    tasks.textContent = newTaskDescription.value;
+    tasksContainer.appendChild(tasks);
+  }
+
+  taskForm.addEventListener("submit", submitForm);
 });
